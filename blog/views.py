16bc425369118b765a9d2c23f72e23bbfe63fc0c
hunnypotpy <sarahdraws@gmail.com>
2021-09-12 16:07:24 -0700

    Worked through Django Starter lesson. Could not get the python manage.py merge at the very end to work, i got an import error for views in my urls.py file. Last step before the crud homework.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib import messages
+from django.shortcuts import get_object_or_404, render, redirect
+
+from .models import Blog
+from .forms import BlogForm
 
 # Create your views here.
+
+def blog_list(request):
+    pass
+
+def blog_detail(request):
+    pass
+
+def blog_new(request):
+    pass
+
+def blog_edit(request):
+    pass
+
+def blog_delete(request):
+    pass
+
+
