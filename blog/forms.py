c50e972a4fcc1d30b40a53ef163f2e4895d30527
Bob Belderbos <bobbelderbos@gmail.com>
2021-09-30 18:19:30 +0200

    fix relations and make migration file

--- blog/forms.py
+++ blog/forms.py
@@ -5,15 +5,16 @@ from .models import Blog, Comment, Image
 class BlogForm(ModelForm):
     class Meta:
         model = Blog
-        fields = ['title', 'post', 'slug', 'cover']
+        fields = ['title', 'post', 'slug']
+
 
 class CommentForm(ModelForm):
     class Meta:
         model = Comment
         fields = ('name', 'body')
 
-class ImageForm(ModelForm):
     #Form for the image model
+class ImageForm(ModelForm):
     class Meta:
         model = Image
         fields = ('title', 'image')
