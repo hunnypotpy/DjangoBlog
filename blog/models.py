fae84a02a285ffa46934be0eb7b17a41026a5ba3
hunnypotpy <sarahdraws@gmail.com>
2021-10-27 22:04:32 -0700

    Hi Bob! Sorry to buy you with a question, Julian mentioned you have the next two days off. No rush checking this! Getting an "django.db.utils.IntegrityError: NOT NULL constraint failed: blog_blog.cover " error when I click "new" to create a new blog test post! I made a change in order to push the code and i added to models.py "IntegrityError at /new
    NOT NULL constraint failed: blog_blog.cover" to the author comment form

--- blog/models.py
+++ blog/models.py
@@ -34,7 +34,7 @@ class Blog(models.Model):
 
 
 class Comment(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null=True, blank=True)
     blog = models.ForeignKey(Blog, on_delete=models.CASCADE, related_name="comments")
     name = models.CharField(max_length=80)
     body = models.TextField()
